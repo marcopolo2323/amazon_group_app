74877d751eaea30881ecb91ef853ea66f8d18228
lloyd <lloyd01forger@gmail.com>
2025-11-13 20:22:13 -0500

    Fix react-native.config.js format

--- frontend/react-native.config.js
+++ frontend/react-native.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   project: {
     android: {
+      sourceDir: './android',
       packageName: 'com.lloyd_forger.amazon_group_app',
+      appName: 'amazon_group_app',
     },
   },
 };
